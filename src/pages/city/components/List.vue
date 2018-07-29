30007852bba4d9dbfbaef4ca9dd05742b56063a4
hj1254123 <hj1254123@qq.com>
2018-07-30 00:37:01 +0800

    临时保存

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+        city
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CityList"
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~@/assets/styles/varibles.styl'
+
+</style>
