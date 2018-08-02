29c4e936ae14e6161008b178dea0fa7425e2ac3e
hj1254123 <hj1254123@qq.com>
2018-08-02 21:02:58 +0800

    完成一半的详情页,以后改为公用组件

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,47 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+            <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
     name: 'Detail',
     components: {
         DetailBanner,
-        DetailHeader
+        DetailHeader,
+        DetailList
+    },
+    data() {
+        return {
+            list: [{
+                title: '一级标题',
+                children: [{
+                    title: '二级标题',
+                    children: [{
+                        title: '三级标题'
+                    }]
+                },{
+                    title: '二级标题',
+                    children: [{
+                        title: '三级标题'
+                    }]
+                }]
+            },{
+                title: '一级标题'
+            },{
+                title: '一级标题'
+            },{
+                title: '一级标题'
+            }]
+        }
     }
 }
 </script>
